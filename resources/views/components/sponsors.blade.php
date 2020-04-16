6921a101c8bdb5572ef4dcf51e8693e5b3769f58
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2020-04-16 22:48:28 +0800

    Change website to 2020

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -32,16 +32,16 @@
             <a href="http://shop.opro9.com/v2/official" class="sponsors oprog"></a>
           </div>
         </div>
-        <div class="col-6 col-lg-3 col-md-6 col-xl-3">
+        {{-- <div class="col-6 col-lg-3 col-md-6 col-xl-3">
           <div class="card card-block">
             <a href="https://www.studioa.com.tw/categories/uag" class="sponsors uag" style="transform: scale(0.6)"></a>
           </div>
-        </div>
-        <div class="col-6 col-lg-3 col-md-6 col-xl-3">
+        </div> --}}
+        {{-- <div class="col-6 col-lg-3 col-md-6 col-xl-3">
           <div class="card card-block">
             <a href="https://rhinoshield.tw/pages/solidsuit" class="sponsors rhino" style="transform: scale(0.6)"></a>
           </div>
-        </div>
+        </div> --}}
         <div class="col-0 col-lg-1 col-sm-0 col-xl-1">
           <div class="card card-block">
           </div>
@@ -51,51 +51,51 @@
 
 
 
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="https://www.audio-technica.com.tw" class="sponsors audiotech"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://www.glowmax.com.tw/product.php?act=list&cid=160" class="sponsors speck"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://tw.pqigroup.com" class="sponsors pqi"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://www.elecom.com.tw" class="sponsors elecom" style="transform: scale(1.5)"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="www.spigen.com.tw" class="sponsors spigen"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="www.ivokamo.com" class="sponsors vokamo"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://www.simplewear.com.tw" class="sponsors nomad"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://maxmikeco.com/" class="sponsors laut"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://www.yingdda.com/" class="sponsors jbl"></a>
           </div>
-        </div>
+        </div> --}}
         <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="https://mytracmo.com/pages/tracmo-taiwan" class="sponsors tracmo"></a>

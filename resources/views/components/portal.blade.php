3ee64ab24bde154114cf80ba33c053bf626c3f1f
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2020-04-21 13:19:48 +0800

    Add two links to home page

--- resources/views/components/portal.blade.php
+++ resources/views/components/portal.blade.php
@@ -2,7 +2,6 @@
     <div class="container-fluid">
         <h2 class="text-center display-4">傳送門</h2>
         <div class="row justify-content-around">
-
             <div class="feature-col col-12 col-md-4">
                 <div class="card card-block text-center" style="cursor: pointer;" onclick="window.location='{{route('team.proposal.uplaod')}}'">
                     <div>
@@ -39,6 +38,34 @@
                     <div>
                         <h3>決賽隊伍 繳交App作品</h3>
                         <p> 決賽隊伍請於 {{Carbon::parse(Setting::get('app_upload_deadline', '2019-7-26'), 'Asia/Taipei')}} 前上傳資料, 詳細內容請<a href="{{route('team.app.uplaod')}}" style="color: #3e48c1;">點我</a>謝謝。 </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-around">
+            <div class="feature-col col-12 col-md-6">
+                <div class="card card-block text-center" style="cursor: pointer;" onclick="window.location= 'www.straighta.com.tw/pages/app' ">
+                    <div>
+                        <div class="feature-icon">
+                            <span class="fas fa-trophy"></span>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>歷屆得獎作品</h3>
+                        {{-- <p> 凡進入決賽隊伍，{{Carbon::parse(Setting::get('register_form_deadline', '2019-6-30'), 'Asia/Taipei')}} 前須提交參賽隊伍所在學校系所用印後的報名表(請至網頁上6/15比賽流程內下載附件)，文件需整併成一個PDF檔，不限制檔案名稱，任一人未於時間內繳交者，則取消該隊決賽資格。</p> --}}
+                    </div>
+                </div>
+            </div>
+            <div class="feature-col col-12 col-md-6">
+                <div class="card card-block text-center" style="cursor: pointer;" onclick="window.location='www.straighta.com.tw/pages/apple-edu'">
+                    <div>
+                        <div class="feature-icon">
+                            <span class="fas fa-apple"></span>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>StraightA購機優惠專區</h3>
+                        {{-- <p> 決賽隊伍請於 {{Carbon::parse(Setting::get('app_upload_deadline', '2019-7-26'), 'Asia/Taipei')}} 前上傳資料, 詳細內容請<a href="{{route('team.app.uplaod')}}" style="color: #3e48c1;">點我</a>謝謝。 </p> --}}
                     </div>
                 </div>
             </div>

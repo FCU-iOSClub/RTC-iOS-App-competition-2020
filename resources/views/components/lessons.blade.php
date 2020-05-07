041a752ae9b2d80d83f7b175fc9cbdbd437504fa
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2020-05-07 13:11:21 +0800

    Fix lessons.blade bug

--- resources/views/components/lessons.blade.php
+++ resources/views/components/lessons.blade.php
@@ -1,8 +1,8 @@
 <section class="lessons">
     <div class="container-fluid text-center px-3 px-md-4 px-lg-5">
-        <h2 class="display-4 mb-3">參賽要求</h2>
+        <h2 class="display-4 mb-3">賽前培訓課程</h2>
     </div>
-    <div class="container-fulid text-center px-xl-5">
+    <div class="container-fulid text-center px-xl-5" style="padding-top: 30px">
         <div class="no-gutters row stats-row justify-content-around justify-content-xl-around">
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
                 <img src="/images/5.11.png" alt="">

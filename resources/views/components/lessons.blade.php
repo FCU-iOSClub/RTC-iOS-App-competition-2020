a019b8d738669217c41c94e0881cdf2d18ef180c
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2020-05-10 23:08:46 +0800

    add video link to image in hame page

--- resources/views/components/lessons.blade.php
+++ resources/views/components/lessons.blade.php
@@ -5,7 +5,9 @@
     <div class="container-fulid text-center px-xl-5" style="padding-top: 30px">
         <div class="no-gutters row stats-row justify-content-around justify-content-xl-around">
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
-                <img src="/images/5.11.png" alt="">
+                <a href={{ route('videos') }}>
+                    <img src="/images/5.11.png" alt="">
+                </a>
             </div>
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
                 <img src="/images/5.12.png" alt="">

cec91e454d730e31475f1e4c0a4606bfa3b49f2c
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2020-05-15 19:48:57 +0800

    Fix bug

--- resources/views/components/lessons.blade.php
+++ resources/views/components/lessons.blade.php
@@ -5,29 +5,19 @@
     <div class="container-fulid text-center px-xl-5" style="padding-top: 30px">
         <div class="no-gutters row stats-row justify-content-around justify-content-xl-around">
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
-                <a href={{ route('videos') }}>
-                    <img src="/images/5.11.png" alt="">
-                </a>
+                <img src="/images/5.11.png" alt="">
             </div>
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
-                <a href={{ route('videos') }}>
-                    <img src="/images/5.12.png" alt="">
-                </a>
+                <img src="/images/5.12.png" alt="">
             </div>
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
-                <a href={{ route('videos') }}>
-                    <img src="/images/5.13.png" alt="">
-                </a>
+                <img src="/images/5.13.png" alt="">
             </div>
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
-                <a href={{ route('videos') }}>
-                    <img src="/images/5.14.png" alt="">
-                </a>
+                <img src="/images/5.14.png" alt="">
             </div>
             <div class="stats-col text-center col-8 col-sm-5 col-md-3 col-xl-2">
-                <a href={{ route('videos') }}>
-                    <img src="/images/5.15.png" alt="">
-                </a>
+                <img src="/images/5.15.png" alt="">
             </div>
         </div>
     </div>

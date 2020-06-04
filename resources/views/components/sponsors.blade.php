34dd85cd29366039a8440caefc56668aec58fbcc
xRockBoyx <kp3344567@mail.fcu.edu.tw>
2020-06-04 15:17:31 +0800

    Fix sponsor bug

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -14,12 +14,12 @@
         </div>
         <div class="col-12 col-lg-4 col-md-12 col-xl-4">
           <div class="card card-block">
-            <a href="https://www.studioa.com.tw/categories/uag" class="sponsors uag"></a>
+            <a href="https://www.studioa.com.tw/categories/uag" class="sponsors uag" style="transform: scale(0.5)"></a>
           </div>
         </div>
         <div class="col-12 col-lg-4 col-md-12 col-xl-4">
           <div class="card card-block">
-            <a href="https://rhinoshield.tw" class="sponsors rhino" style="transform: scale(0.6)"></a>
+            <a href="https://rhinoshield.tw" class="sponsors rhino" style="transform: scale(1.1)"></a>
           </div>
         </div>
         
@@ -49,47 +49,47 @@
           <div class="card card-block">
           </div>
         </div> --}}
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="http://www.jbl-tw.com/" class="sponsors jbl"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="https://tw.pqigroup.com/" class="sponsors pqi"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="http://www.elecom.com.tw/" class="sponsors elecom"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="https://www.spigen.com.tw/" class="sponsors spigen"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="https://tw.gigastone.com/" class="sponsors gigastone"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="http://vago.global/" class="sponsors vago"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="http://smarthome.myvita.com.tw/" class="sponsors infinite"></a>
           </div>
         </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        {{-- <div class="col-4 col-lg-2 col-md-4 col-xl-2">
           <div class="card card-block">
             <a href="http://maxmikeco.com/" class="sponsors laut"></a>
           </div>
-        </div>
-        <div class="col-4 col-lg-2 col-md-4 col-xl-2">
+        </div> --}}
+        <div class="col-4 col-lg-4 col-md-4 col-xl-4">
           <div class="card card-block">
             <a href="http://litenet.com.tw/" class="sponsors litenet"></a>
           </div>
